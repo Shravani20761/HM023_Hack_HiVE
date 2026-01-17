9c0496e2585a2d47e914e6ef5e3e644d9eda28cb
Shravani <shravanijadhav921@gmail.com>
2026-01-17 16:06:33 +0530

    shravavni

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,16 @@
-// import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './pages/login';
+import Home from './pages/home';
 
 
 function App() {
   return (
-    <>
-     
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Router>
   )
 }
 
